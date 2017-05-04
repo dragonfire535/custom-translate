049f1be60225b328e4135df6f608900984f7ba0a
Daniel Odendahl Jr <danielbodendahl@gmail.com>
2017-05-04 03:04:43 +0000

    Two Tiny Spaces

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 module.exports.wordTrans = (text, words) => {
     return text.split(' ').map(word => {
         const strip = word.replace(/[\[\\^$.,:;|!?%#@"*+()\]]/g, '').toLowerCase();
-        if (words[strip]) return word.toLowerCase().replace(strip, words[strip]);
+        if(words[strip]) return word.toLowerCase().replace(strip, words[strip]);
         else return word;
     }).join(' ');
 };
 
 module.exports.letterTrans = (text, letters, joinWith) => {
     return text.split('').map(letter => {
-        if (letters[letter]) return letters[letter];
+        if(letters[letter]) return letters[letter];
         else return letter;
     }).join(joinWith || '');
 };
