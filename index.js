fa723b5ae36849bab9c33373400ce9fcfb8294d3
Daniel Odendahl Jr <danielbodendahl@gmail.com>
2017-10-03 01:13:38 +0000

    Validation, fix up caps change

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
 const { version } = require('./package');
 
-const wordTrans = (text, dict, join = ' ') => text.split(' ').map(word => {
-	const strip = word.replace(/[!@#$%^&*()`~=+[\]{};:",.<>?]/g, '').toLowerCase();
-	if (!dict[strip]) return word;
-	let change = word.toLowerCase().replace(strip, dict[strip]);
-	if (word.charAt(0).toUpperCase() === word.charAt(0)) change = change.replace(/./, change.charAt(0).toUpperCase());
-	return change;
-}).join(join);
+const wordTrans = (text, dict, join = ' ') => {
+	if (typeof text !== 'string') throw new TypeError('text must be a string.');
+	if (typeof dict !== 'object') throw new TypeError('dictionary must be an object.');
+	return text.split(' ').map(word => {
+		const strip = word.replace(/[!@#$%^&*()`~=+[\]{};:",.<>?]/g, '').toLowerCase();
+		if (!dict[strip]) return word;
+		let change = word.toLowerCase().replace(strip, dict[strip]);
+		if (word.charAt(0).toUpperCase() === word.charAt(0)) {
+			change = change.replace(dict[strip].charAt(0), dict[strip].charAt(0).toUpperCase());
+		}
+		return change;
+	}).join(join);
+};
 
-const letterTrans = (text, dict, join = '') => text.split('').map(letter => dict[letter] || letter).join(join);
+const letterTrans = (text, dict, join = '') => {
+	if (typeof text !== 'string') throw new TypeError('text must be a string.');
+	if (typeof dict !== 'object') throw new TypeError('dictionary must be an object.');
+	return text.split('').map(letter => dict[letter] || letter).join(join);
+};
 
 module.exports = {
 	version,
