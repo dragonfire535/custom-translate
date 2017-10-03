29c2825785390fa492925c89cc25d28c7d17b367
Daniel Odendahl Jr <danielbodendahl@gmail.com>
2017-10-03 01:22:11 +0000

    FINALLY FIX THE CASING PROBLEM

--- index.js
+++ index.js
@@ -4,11 +4,12 @@ const wordTrans = (text, dict, join = ' ') => {
 	if (typeof text !== 'string') throw new TypeError('text must be a string.');
 	if (typeof dict !== 'object') throw new TypeError('dictionary must be an object.');
 	return text.split(' ').map(word => {
-		const strip = word.replace(/[!@#$%^&*()`~=+[\]{};:",.<>?]/g, '').toLowerCase();
-		if (!dict[strip]) return word;
-		let change = word.toLowerCase().replace(strip, dict[strip]);
-		if (word.charAt(0).toUpperCase() === word.charAt(0)) {
-			change = change.replace(dict[strip].charAt(0), dict[strip].charAt(0).toUpperCase());
+		const strip = word.replace(/[!@#$%^&*()`~=+[\]{};:",.<>?]/g, '');
+		const lowerCase = strip.toLowerCase();
+		if (!dict[lowerCase]) return word;
+		let change = word.toLowerCase().replace(lowerCase, dict[lowerCase]);
+		if (strip.charAt(0).toUpperCase() === strip.charAt(0)) {
+			change = change.replace(dict[lowerCase].charAt(0), dict[lowerCase].charAt(0).toUpperCase());
 		}
 		return change;
 	}).join(join);
